74b5258fd6f8a379fed2315e0e10a6e22e07ac03
Enrica Fedeli <enricafedeli@gmail.com>
2020-12-18 06:44:03 +0000

    up to advanced routing, chapter 10

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,8 +1,17 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class BandsBandSongsRoute extends Route {
-  model() {
+  @service catalog;
+
+  async model() {
     let band = this.modelFor('bands.band');
-    return band.songs;
+    await this.catalog.fetchRelated(band, 'songs');
+    return band;
+  }
+  
+  resetController(controller) {
+    controller.title = '';
+    controller.showAddSong = true;
   }
 }
